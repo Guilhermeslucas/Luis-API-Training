c260c6d19bc1385bebdc2ef8508eea1c9c978a8f
Guilherme Lucas <guilherme.slucas@gmail.com>
2018-04-05 11:24:50 -0300

    adding entity via REST request

---
+++ Python/creatingEntity.py
@@ -0,0 +1,21 @@
+########### Python 3.2 #############
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+
+headers = {
+    # Request headers
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': '',
+}
+
+params = urllib.parse.urlencode({
+})
+
+try:
+    conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
+    conn.request("POST", "/luis/api/v2.0/apps/a4d2dae2-f847-4f86-8b17-147b90da0040/versions/0.1/entities?%s" % params, "{'name':'dayOfWeek'}", headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))

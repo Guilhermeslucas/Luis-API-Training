08bd789a7926aef65bdebdf0113b7eb68c3b6752
Guilherme Lucas <guilherme.slucas@gmail.com>
2018-04-05 14:34:50 -0300

    adding utterances programatically

--- Python/addingUterances.py
+++ Python/addingUterances.py
@@ -8,15 +8,17 @@ headers = {
     'Ocp-Apim-Subscription-Key': '',
 }
 
-params = urllib.parse.urlencode({
-})
+data = open('utterances.json', encoding='UTF8')
+data = data.read()
 
 try:
     conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
-    conn.request("POST", "/luis/api/v2.0/apps/a4d2dae2-f847-4f86-8b17-147b90da0040/versions/0.1/entities?%s" % params, "{'name':'dayOfWeek'}", headers)
+    conn.request("POST", "/luis/api/v2.0/apps/a4d2dae2-f847-4f86-8b17-147b90da0040/versions/0.1/examples", data.encode('UTF8'), headers)
     response = conn.getresponse()
     data = response.read()
     print(data)
     conn.close()
 except Exception as e:
     print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+
